8966968f16f4a85b0544dd6c18e08f67957ae5d9
a-rin <kitosiro2@gmail.com>
2018-07-24 08:15:02 +0900

    sent_confirmation, set_as_primaryのviewを分離

--- drf_allauthmail/views.py
+++ drf_allauthmail/views.py
@@ -2,12 +2,13 @@ from allauth.account import signals
 from allauth.account.models import EmailAddress
 from allauth.account.utils import sync_user_email_addresses
 from django.utils.translation import ugettext as _
-from rest_framework.decorators import action
 from rest_framework.exceptions import ValidationError
+from rest_framework.generics import get_object_or_404
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, CreateModelMixin, DestroyModelMixin
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.serializers import Serializer
+from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
 
 from .serializers import EmailAddressSerializer
@@ -59,8 +60,34 @@ class EmailViewSet(
                                        user=self.request.user,
                                        email_address=email_address)
 
-    @action(methods=['put'], detail=True)
-    def set_as_primary(self, request, pk=None):
+
+class EmailSubActionAPIView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        queryset = self.request.user.emailaddress_set.filter(id=self.kwargs['pk'])
+        obj = get_object_or_404(queryset)
+
+        # May raise a permission denied
+        self.check_object_permissions(self.request, obj)
+
+        return obj
+
+
+class SentConfirmationView(EmailSubActionAPIView):
+
+    def post(self, request, *args, **kwargs):
+        """
+        send confirmation e-mail to a specific e-mail address
+        """
+
+        email_address = self.get_object()
+        email_address.send_confirmation(self.request)
+        return Response({_('Confirmation e-mail sent to the e-mail address')})
+
+
+class SetAsPrimaryView(EmailSubActionAPIView):
+    def put(self, request, *args, **kwargs):
         """
         set a specific e-mail as primary
         """
@@ -84,18 +111,8 @@ class EmailViewSet(
                       user=request.user,
                       from_email_address=from_email_address,
                       to_email_address=email_address)
-            serializer = self.get_serializer(instance=email_address)
+            serializer = EmailAddressSerializer(instance=email_address, user=request.user)
             return Response(serializer.data)
-
-    @action(methods=['post'], detail=True)
-    def sent_confirmation(self, request, pk=None):
-        """
-        send confirmation e-mail to a specific e-mail address
-        """
-
-        email_address = self.get_object()
-        email_address.send_confirmation(request)
-        return Response({_('Confirmation e-mail sent to the e-mail address')})
 
 
 email_list_view = EmailViewSet.as_view(
@@ -105,10 +122,5 @@ email_detail_view = EmailViewSet.as_view(
     {'get': 'retrieve', 'delete': 'destroy'}
 )
 
-email_set_primary = EmailViewSet.as_view(
-    {'put': 'set_as_primary'}
-)
-
-email_sent_confirmation = EmailViewSet.as_view(
-    {'post': 'sent_confirmation'}
-)
+email_set_primary = SetAsPrimaryView.as_view()
+email_sent_confirmation = SentConfirmationView.as_view()
